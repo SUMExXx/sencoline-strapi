27a653edf6a38d43f6801e23078247da647bc2e7
Suman Debnath <54632628+SUMExXx@users.noreply.github.com>
2025-09-30 12:50:26 +0530

    production fix

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,10 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_STRAPI_URL'),
+  admin: {
+    url: env('SUB_PATH') + '/admin',
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
