734e3b147997b1e41196fb5a9e3823c86b987af5
Suman Debnath <54632628+SUMExXx@users.noreply.github.com>
2025-10-01 13:27:30 +0530

    updated middleware csp

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,22 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'default-src': ["'self'", 'https://strapi.sencoline.com'],
+          'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'blob:', 'https://strapi.sencoline.com'],
+          'style-src': ["'self'", "'unsafe-inline'", 'https://strapi.sencoline.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://strapi.sencoline.com'],
+          'connect-src': ["'self'", 'https://strapi.sencoline.com', 'https:', 'ws:', 'wss:'],
+          'font-src': ["'self'", 'https://strapi.sencoline.com'],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
